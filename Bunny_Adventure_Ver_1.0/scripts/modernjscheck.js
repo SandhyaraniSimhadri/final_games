7a01951dce2d8fc626e622ae36e312846e5aeb60
sandhyasimhadri <sandhyasimhadri999@gmail.com>
2024-10-03 15:10:18 +0530

    game

--- Bunny_Adventure_Ver_1.0/scripts/modernjscheck.js
+++ Bunny_Adventure_Ver_1.0/scripts/modernjscheck.js
@@ -1,1 +1,14 @@
-'use strict';(function(){let pass=true;if((null??2)!==2)pass=false;if(pass)window["C3_ModernJSSupport_OK"]=true})();
+'use strict';
+
+(function() {
+    let pass = true;
+
+    // Example with a variable that could be nullish
+    let testValue;  // This is `undefined` initially
+    if ((testValue ?? 2) !== 2) pass = false;
+
+    // Conditionally assign if pass is true
+    if (pass) {
+        window["C3_ModernJSSupport_OK"] = true;
+    }
+})();
