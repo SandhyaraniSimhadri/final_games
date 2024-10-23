3ae7fed320a9bd5cde4b30c632753d5eb44d2a9e
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2024-10-23 18:28:16 +0530

    games

--- Bunny_Adventure_Ver_1.0_123/scripts/opus.wasm.js
+++ Bunny_Adventure_Ver_1.0_123/scripts/opus.wasm.js
@@ -937,51 +937,67 @@ function stringToAscii(str, outPtr) {
 }
 Module["stringToAscii"] = stringToAscii;
 let UTF8Decoder = typeof TextDecoder !== "undefined" ? new TextDecoder("utf8") : undefined;
-
 function UTF8ArrayToString(u8Array, idx) {
+    let endPtr = findStringEnd(u8Array, idx);
+    
+    if (endPtr - idx > 16 && u8Array.subarray && UTF8Decoder) {
+        return UTF8Decoder.decode(u8Array.subarray(idx, endPtr));
+    } else {
+        return decodeUTF8(u8Array, idx, endPtr);
+    }
+}
+
+function findStringEnd(u8Array, idx) {
     let endPtr = idx;
     while (u8Array[endPtr]) ++endPtr;
-    if (endPtr - idx > 16 && u8Array.subarray && UTF8Decoder) {
-        return UTF8Decoder.decode(u8Array.subarray(idx, endPtr))
+    return endPtr;
+}
+
+function decodeUTF8(u8Array, idx, endPtr) {
+    let str = "";
+    while (idx < endPtr) {
+        let u0 = u8Array[idx++];
+        if (!u0) return str;
+        str += decodeCodePoint(u8Array, u0, idx);
+    }
+    return str;
+}
+
+function decodeCodePoint(u8Array, u0, idx) {
+    if (!(u0 & 128)) return String.fromCharCode(u0);
+    
+    let u1 = u8Array[idx++] & 63;
+    if ((u0 & 224) === 192) {
+        return String.fromCharCode((u0 & 31) << 6 | u1);
+    }
+    
+    let u2 = u8Array[idx++] & 63;
+    if ((u0 & 240) === 224) {
+        u0 = (u0 & 15) << 12 | u1 << 6 | u2;
     } else {
-        let u0, u1, u2, u3, u4, u5;
-        let str = "";
-        while (1) {
-            u0 = u8Array[idx++];
-            if (!u0) return str;
-            if (!(u0 & 128)) {
-                str += String.fromCharCode(u0);
-                continue
-            }
-            u1 = u8Array[idx++] & 63;
-            if ((u0 & 224) == 192) {
-                str += String.fromCharCode((u0 & 31) << 6 | u1);
-                continue
-            }
-            u2 = u8Array[idx++] & 63;
-            if ((u0 & 240) == 224) {
-                u0 = (u0 & 15) << 12 | u1 << 6 | u2
+        let u3 = u8Array[idx++] & 63;
+        if ((u0 & 248) === 240) {
+            u0 = (u0 & 7) << 18 | u1 << 12 | u2 << 6 | u3;
+        } else {
+            let u4 = u8Array[idx++] & 63;
+            if ((u0 & 252) === 248) {
+                u0 = (u0 & 3) << 24 | u1 << 18 | u2 << 12 | u3 << 6 | u4;
             } else {
-                u3 = u8Array[idx++] & 63;
-                if ((u0 & 248) == 240) {
-                    u0 = (u0 & 7) << 18 | u1 << 12 | u2 << 6 | u3
-                } else {
-                    u4 = u8Array[idx++] & 63;
-                    if ((u0 & 252) == 248) {
-                        u0 = (u0 & 3) << 24 | u1 << 18 | u2 << 12 | u3 << 6 | u4
-                    } else {
-                        u5 = u8Array[idx++] & 63;
-                        u0 = (u0 & 1) << 30 | u1 << 24 | u2 << 18 | u3 << 12 | u4 << 6 | u5
-                    }
-                }
-            }
-            if (u0 < 65536) {
-                str += String.fromCharCode(u0)
-            } else {
-                let ch = u0 - 65536;
-                str += String.fromCharCode(55296 | ch >> 10, 56320 | ch & 1023)
+                let u5 = u8Array[idx++] & 63;
+                u0 = (u0 & 1) << 30 | u1 << 24 | u2 << 18 | u3 << 12 | u4 << 6 | u5;
             }
         }
+    }
+    
+    return createUTF16Pair(u0);
+}
+
+function createUTF16Pair(u0) {
+    if (u0 < 65536) {
+        return String.fromCharCode(u0);
+    } else {
+        let ch = u0 - 65536;
+        return String.fromCharCode(55296 | (ch >> 10), 56320 | (ch & 1023));
     }
 }
 Module["UTF8ArrayToString"] = UTF8ArrayToString;
