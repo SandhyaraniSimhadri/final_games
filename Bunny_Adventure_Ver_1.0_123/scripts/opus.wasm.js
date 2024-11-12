dbf79e47e5ea0277001ac56a8e2c303edee4737a
Sandhya Rani Simhadri <sandhyasimhadri999@gmail.com>
2024-11-12 15:46:30 +0530

    games

--- Bunny_Adventure_Ver_1.0_123/scripts/opus.wasm.js
+++ Bunny_Adventure_Ver_1.0_123/scripts/opus.wasm.js
@@ -1865,25 +1865,25 @@ Module.asmLibraryArg = {
 let asm = Module["asm"](Module.asmGlobalArg, Module.asmLibraryArg, buffer);
 Module["asm"] = asm;
 
-Module["_malloc"] = (function() {
+var _malloc = Module["_malloc"] = (function() {
     return Module["asm"]["_malloc"].apply(null, arguments)
 });
 
 Module["_free"] = function() {
     return Module["asm"]["_free"].apply(null, arguments);
 };
-Module["_memcpy"] = (function() {
+var _memcpy = Module["_memcpy"] = (function() {
     return Module["asm"]["_memcpy"].apply(null, arguments)
 });
 
-Module["_memmove"] = (function() {
+var _memmove = Module["_memmove"] = (function() {
     return Module["asm"]["_memmove"].apply(null, arguments)
 });
 
-Module["_memset"] = (function() {
+var _memset = Module["_memset"] = (function() {
     return Module["asm"]["_memset"].apply(null, arguments)
 });
-Module["_sbrk"] = (function() {
+var _sbrk = Module["_sbrk"] = (function() {
     return Module["asm"]["_sbrk"].apply(null, arguments)
 });
 
